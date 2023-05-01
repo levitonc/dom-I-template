344cc211d2fbf3452a340176b5ac178a85664460
Léviton Lima Carvalho <levitonc@live.com>
2023-05-01 13:13:27 -0300

    templates aula dom'

---
+++ pratica2/script.js
@@ -0,0 +1,24 @@
+//CAPTURANDO O ELEMENTO HTML
+
+const inputNome = document.getElementById('nome')
+
+// IMPRIMINDO O VALUE NO INPUT
+
+console.log(inputNome.value)
+
+//LIMPANDO O INPUT 
+
+inputNome.value = ''
+
+const inputEndereco = document.getElementById('endereco')
+
+console.log(inputEndereco.value)
+
+inputEndereco.value = ''
+
+const inputEmail = document.getElementById('email')
+
+console.log(inputEmail.value)
+
+inputEmail.value = ''
+
---
+++ pratica3/script.js
@@ -0,0 +1,6 @@
+let contador = 0
+
+const contaClick = () => {
+    contador += 1
+    console.log(`O botão foi clickado ${contador} vezes`)
+}
